3c61e9a74d84e4f0f12aefa08befbc9baf409e3b
Daniel Moore <dmoore09@vt.edu>
2014-02-05 20:44:10 -0500

    Changed processes.h so lists can be made

--- processes.h
+++ processes.h
@@ -8,12 +8,18 @@ typedef struct Process {
 	//process id
 	int pid;
 	//current state will be used in commands like ps, jobs
-	String state;
+	// 0 running fg
+	// 1 stopped fg
+	// 2 running bg
+	// 3 stopped bg
+	int state;
 	
+	//is part of a list of processes
+	struct list_elem elem;
 };
 
 /* Structure that represents a process group. It contains a linked
    list groups that contains all processes in the group*/
 typedef struct Process_Group {
-	struct list groups;
+
 };
